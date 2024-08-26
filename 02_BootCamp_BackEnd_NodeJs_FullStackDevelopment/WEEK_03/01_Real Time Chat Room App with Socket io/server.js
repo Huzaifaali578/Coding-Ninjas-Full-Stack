edd7702ba80096f4939ebec4ae2748e2efe7b767
Huzaifa Nagariya <huzaifaali578@gmail.com>
2024-08-26 16:12:55 +0530

    Initial commit

---
+++ 02_BootCamp_BackEnd_NodeJs_FullStackDevelopment/WEEK_03/01_Real Time Chat Room App with Socket io/server.js
@@ -0,0 +1,52 @@
+// No need to change the pre-written code
+// Implement the features in io.on() section
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+
+export const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ["GET", "POST"]
+    }
+});
+
+io.on("connection", (socket) => {
+    console.log("Connection made.");
+
+    // Write your code here
+    socket.on("join", (data)=> {
+        // welcome message to the user who joined
+        socket.emit("message", {text: `Welcome, ${data.username}!`});
+
+        // broadcast the message to all other users in same room
+        socket.broadcast.to(data.room).emit("message", {
+            text: `${data.username} has joined the room.`
+        });
+
+        // join the room
+        socket.join(data.room);
+    });
+
+
+    socket.on("sendMessage", async (data) => {
+        // broadcast the recived message to all user in the same room
+        io.to(data.room).emit("message", {
+            username: data.username,
+            text: data.message
+        });
+    });
+
+
+    socket.on("disconnect", () => {
+        console.log("Connection disconnected.");
+    });
+});
+
+export default server;
